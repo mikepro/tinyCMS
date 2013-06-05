d205b1d52f7b0bc85ff579a0b0338ca2b12a1c83
Marcin Floryan <marcin.floryan@gmail.com>
2013-06-05 23:34:15 +0100

    Serve a list of aggregated brands

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var repository = require('./lib/brandRepository');
 
 var app = express();
+app.get('/brands/list.json',function(req, res) {
+  repository.list(res);
+});
 app.use(express.static(__dirname +'/content'));
 app.listen(8022);
