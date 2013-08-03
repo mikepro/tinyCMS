7069b07d0683ceb20d67aef4960a4c90b241723e
Marcin Floryan <marcin.floryan@gmail.com>
2013-08-03 21:55:45 +0100

    Data can be imported directly over the web now

--- import.js
+++ import.js
@@ -1,62 +1,85 @@
 
 var fs = require('fs'),
+    request = require('request'),
     mongo = require('mongodb');
 
 console.log("Importing data");
 
-var data = fs.readFileSync('brands.json');
-var brands = JSON.parse(data);
+var dataSource = process.argv[2] || "https://home.comparethemarket.com/brands";
 
-function CleanupBrandData(brandData) {
-  brandData.brandCode = brandData.Name;
-  delete brandData.Name;
+var data;
 
-  brandData.Benefits = brandData.brandBenefits.Benefits;
-  delete brandData.brandBenefits;
-
-  delete brandData.features;
-  delete brandData.LogoUrl;
-  if (brandData.offlinePresence) {
-    if (brandData.offlinePresence.OpeningTimes) {
-      delete brandData.offlinePresence.OpeningTimes.Name;
+if (/^https?:\/\//.test(dataSource)) {
+  console.log("Loading data over the web from: " + dataSource);
+  request({uri: dataSource}, function(err, response, body) {
+    if (!err) {
+      processData(body);
     }
-    delete brandData.offlinePresence.Name;
-  }
-  delete brandData.offer.Name;
-
-  return brandData;
+  })
+} else {
+  console.log("Loading data from file: " + dataSource);
+  data = fs.readFile('brands.json', function(err, data) {
+    if (!err) {
+      processData(data);
+    }
+  });
 }
 
-mongo.MongoClient.connect("mongodb://localhost:27017/tinyCMS", function(err, db) {
-  if(!err) {
-    console.log("We are connected");
+var processData = function(data) {
+
+  function CleanupBrandData(brandData) {
+    brandData.brandCode = brandData.Name;
+    delete brandData.Name;
+
+    brandData.Benefits = brandData.brandBenefits.Benefits;
+    delete brandData.brandBenefits;
+
+    delete brandData.features;
+    delete brandData.LogoUrl;
+    if (brandData.offlinePresence) {
+      if (brandData.offlinePresence.OpeningTimes) {
+        delete brandData.offlinePresence.OpeningTimes.Name;
+      }
+      delete brandData.offlinePresence.Name;
+    }
+    delete brandData.offer.Name;
+
+    return brandData;
   }
 
-  db.createCollection('brands', function(err, collection) {
-    if (err) {
-      console.log(err);
+  var brands = JSON.parse(data);
+
+  mongo.MongoClient.connect("mongodb://localhost:27017/tinyCMS", function(err, db) {
+    if(!err) {
+      console.log("We are connected");
     }
 
-    var i = 0;
-
-    collection.createIndex({brandCode:1, status:1}, {unique:true, w:1}, function(error, indexName) {
-      if (!err) {
-        console.log('Index created: ' + indexName);
+    db.createCollection('brands', function(err, collection) {
+      if (err) {
+        console.log(err);
       }
 
-      brands.Brands.forEach(function(brandData) {
-        var brand = CleanupBrandData(brandData);
-        brand.status = "published";
-        brand.createdAt = new Date();
+      var i = 0;
 
-        collection.insert(brand, function(err, r) {
-          if (!err) {
-            console.log('*' + brand.brandCode + ' (' + i++ + ')');
-          } else {
-            console.log(err.err);
-          }
+      collection.createIndex({brandCode:1}, {unique:true, w:1}, function(error, indexName) {
+        if (!err) {
+          console.log('Index created: ' + indexName);
+        }
+
+        brands.Brands.forEach(function(brandData) {
+          var brand = CleanupBrandData(brandData);
+          brand.status = "published";
+          brand.createdAt = new Date();
+
+          collection.insert(brand, function(err, r) {
+            if (!err) {
+              console.log('*' + brand.brandCode + ' (' + i++ + ')');
+            } else {
+              console.log(err.err);
+            }
+          });
         });
       });
     });
   });
-});
+};
