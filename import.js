8bddbed34cbd99168c77872b2b37060abe9ac59d
Marcin Floryan <marcin.floryan@gmail.com>
2013-08-03 22:25:22 +0100

    Importing data into a new data structure

--- import.js
+++ import.js
@@ -1,33 +1,14 @@
+"use strict";
 
-var fs = require('fs'),
-    request = require('request'),
-    mongo = require('mongodb');
+var
+  fs = require('fs'),
+  request = require('request'),
+  mongo = require('mongodb');
 
-console.log("Importing data");
 
-var dataSource = process.argv[2] || "https://home.comparethemarket.com/brands";
+var processData = function (data) {
 
-var data;
-
-if (/^https?:\/\//.test(dataSource)) {
-  console.log("Loading data over the web from: " + dataSource);
-  request({uri: dataSource}, function(err, response, body) {
-    if (!err) {
-      processData(body);
-    }
-  })
-} else {
-  console.log("Loading data from file: " + dataSource);
-  data = fs.readFile('brands.json', function(err, data) {
-    if (!err) {
-      processData(data);
-    }
-  });
-}
-
-var processData = function(data) {
-
-  function CleanupBrandData(brandData) {
+  function cleanupBrandData(brandData) {
     brandData.brandCode = brandData.Name;
     delete brandData.Name;
 
@@ -43,37 +24,46 @@ var processData = function(data) {
       delete brandData.offlinePresence.Name;
     }
     delete brandData.offer.Name;
-
-    return brandData;
   }
 
   var brands = JSON.parse(data);
 
-  mongo.MongoClient.connect("mongodb://localhost:27017/tinyCMS", function(err, db) {
-    if(!err) {
+  mongo.MongoClient.connect("mongodb://localhost:27017/tinyCMS", function (err, db) {
+    if (!err) {
       console.log("We are connected");
     }
 
-    db.createCollection('brands', function(err, collection) {
+    db.createCollection('brands', function (err, collection) {
       if (err) {
         console.log(err);
       }
 
-      var i = 0;
+      var i = 1;
 
-      collection.createIndex({brandCode:1}, {unique:true, w:1}, function(error, indexName) {
+      collection.createIndex({"code" : 1}, {"unique" : true}, function (error, indexName) {
         if (!err) {
           console.log('Index created: ' + indexName);
         }
 
-        brands.Brands.forEach(function(brandData) {
-          var brand = CleanupBrandData(brandData);
-          brand.status = "published";
-          brand.createdAt = new Date();
+        brands.Brands.forEach(function (brandData) {
+          cleanupBrandData(brandData);
 
-          collection.insert(brand, function(err, r) {
+          brandData.created = {data: new Date(), user: 'import'};
+
+          var brandDocument = {
+            code: brandData.brandCode,
+            published: brandData
+          };
+
+          delete brandData.brandCode;
+
+          collection.insert(brandDocument, function (err) {
             if (!err) {
-              console.log('*' + brand.brandCode + ' (' + i++ + ')');
+              i = i + 1;
+              process.stdout.write(brandDocument.code + ' (' + i + ') ');
+              if (!(i % 10)) {
+                process.stdout.write("\n");
+              }
             } else {
               console.log(err.err);
             }
@@ -83,3 +73,26 @@ var processData = function(data) {
     });
   });
 };
+
+console.log("Importing data");
+
+var dataSource = process.argv[2] || "https://home.comparethemarket.com/brands";
+
+var data;
+
+if (/^https?:\/\//.test(dataSource)) {
+  console.log("Loading data over the web from: " + dataSource);
+  request({uri: dataSource}, function (err, response, body) {
+    if (!err) {
+      processData(body);
+    }
+  });
+} else {
+  console.log("Loading data from file: " + dataSource);
+  data = fs.readFile('brands.json', function (err, data) {
+    if (!err) {
+      processData(data);
+    }
+  });
+}
+
