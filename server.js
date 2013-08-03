63632360425c6bf57957765f4f177036a33bb1dd
Marcin Floryan <marcin.floryan@gmail.com>
2013-08-03 20:40:48 +0100

    Applied JSLint to server.js

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
+"use strict";
+
 var express = require('express');
 var repository = require('./lib/brandRepository');
 
 var app = express();
 app.use(express.bodyParser());
 
-app.get('/brands/list.json',function(req, res) {
+app.get('/brands/list.json', function (req, res) {
   repository.list(res);
 });
 
-app.get('/brands/get.json', function(req, res) {
+app.get('/brands/get.json', function (req, res) {
   if (req.query.code) {
     repository.get(req.query.code, res);
   } else {
-    res.send(400, {error:'A query parameter "code" is required'});
+    res.send(400, {error: 'A query parameter "code" is required'});
   }
 });
 
-app.post('/brands/save', function(req, res) {
+app.post('/brands/save', function (req, res) {
   if (!req.body) {
     res.send(400);
     return;
@@ -24,7 +26,7 @@ app.post('/brands/save', function(req, res) {
   repository.save(req.body, res);
 });
 
-app.post('/brands/publish', function(req, res) {
+app.post('/brands/publish', function (req, res) {
   if (!req.body) {
     res.send(400);
     return;
@@ -32,9 +34,9 @@ app.post('/brands/publish', function(req, res) {
   repository.publish(req.body, res);
 });
 
-app.get('/brands/published.json', function(req, res) {
+app.get('/brands/published.json', function (req, res) {
   repository.published(res);
 });
 
-app.use(express.static(__dirname +'/content'));
+app.use(express.static(__dirname + '/content'));
 app.listen(8022);
