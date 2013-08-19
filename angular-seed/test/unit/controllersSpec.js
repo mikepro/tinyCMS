f6fba3db6236b0a7d2cf9cc6505094baba5cb83c
mwatson <mwatson@someemailthatdoesnotexist.com>
2013-08-19 22:28:27 +0100

    Adding angular js seed to the project
    Available from https://github.com/angular/angular-seed

---
+++ angular-seed/test/unit/controllersSpec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/* jasmine specs for controllers go here */
+
+describe('controllers', function(){
+  beforeEach(module('myApp.controllers'));
+
+
+  it('should ....', inject(function() {
+	  expect(4+1).toBe(5);
+  }));
+
+  it('should ....', inject(function() {
+    //spec body
+  }));
+});
